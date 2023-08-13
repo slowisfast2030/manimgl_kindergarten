7c62e85cc538a1f5af4ecea49ee36ca5584ffbbf
Ai Chain <hitthumit@gmail.com>
2023-08-13 21:10:02 +0800

    slow is fast

--- manimlib/shaders/inserts/position_point_into_frame.glsl
+++ manimlib/shaders/inserts/position_point_into_frame.glsl
@@ -3,6 +3,12 @@
 // uniform vec3 camera_offset;
 // uniform mat3 camera_rotation;
 
+/*
+将一个点从世界空间转换到相机空间
+将点减去相机偏移量并将其旋转到相机方向
+
+相机偏移量是相机在世界空间中的位置
+*/
 vec3 rotate_point_into_frame(vec3 point){
     if(bool(is_fixed_in_frame)){
         return point;
