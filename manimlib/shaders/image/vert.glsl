9a45eacb131f648e65ec80f0698a398da3bd7428
Ai Chain <hitthumit@gmail.com>
2023-08-13 21:42:25 +0800

    slow is fast

--- manimlib/shaders/image/vert.glsl
+++ manimlib/shaders/image/vert.glsl
@@ -46,6 +46,7 @@ void main(){
     /*
     gl_Position in the vertex shader is typically expected to be in clip space, 
     which ranges from -1 to 1 in all three dimensions.
+    ???
     */
     gl_Position = get_gl_Position(position_point_into_frame(point));
 }
