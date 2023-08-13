c48295094a66dfd1c2c8744314d350ef69317695
Ai Chain <hitthumit@gmail.com>
2023-08-13 21:07:07 +0800

    slow is fast

--- manimlib/shaders/image/vert.glsl
+++ manimlib/shaders/image/vert.glsl
@@ -31,12 +31,15 @@ out vec2 v_im_coords;
 out float v_opacity;
 
 // Analog of import for manim only
+/*
+get_gl_Position函数将点从相机空间转换为裁剪空间。
+*/
 #INSERT get_gl_Position.glsl
+/*
+position_point_into_frame函数将点从世界空间转换为相机空间。
+*/
 #INSERT position_point_into_frame.glsl
 
-// 一个猜想：这里的point已经在world space
-// 通过position_point_into_frame把point转换到camera space
-// 通过get_gl_Position把camera space的point转换到clip space
 void main(){
     v_im_coords = im_coords;
     v_opacity = opacity;
